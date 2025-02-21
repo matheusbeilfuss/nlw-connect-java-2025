c0a75babdc8ef76950d25518e915695faa30789b
Beilfuss <matheusdicimb@gmail.com>
2025-02-20 22:16:32 -0300

    ranking de indicacoes

--- src/main/java/br/com/nlw/events/controller/SubscriptionController.java
+++ src/main/java/br/com/nlw/events/controller/SubscriptionController.java
@@ -2,6 +2,7 @@ package br.com.nlw.events.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,6 +43,28 @@ public class SubscriptionController {
 			return ResponseEntity.status(404).body(new ErrorMessage(ex.getMessage()));
 		}
 		return ResponseEntity.badRequest().build();
+		
+	}
+	
+	@GetMapping("/subscription/{prettyName}/ranking")
+	public ResponseEntity<?> generateRankingByEvent(@PathVariable String prettyName) {
+		try {
+			return ResponseEntity.ok(service.getCompleteRanking(prettyName).subList(0, 3));
+		} catch(EventNotFoundException e) {
+			return ResponseEntity.status(404).body(new ErrorMessage(e.getMessage()));
+		}
+		
+	}
+	
+	@GetMapping("/subscription/{prettyName}/ranking/{userId}")
+	public ResponseEntity<?> generateRankingByEventAndUser(@PathVariable String prettyName,
+														   @PathVariable Integer userId) {
+		
+		try {
+			return ResponseEntity.ok(service.getRankingByUser(prettyName, userId));
+		} catch (Exception ex) {
+			return ResponseEntity.status(404).body(new ErrorMessage(ex.getMessage()));
+		}
 		
 	}
 	
--- src/main/java/br/com/nlw/events/service/SubscriptionService.java
+++ src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -1,8 +1,13 @@
 package br.com.nlw.events.service;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.nlw.events.dto.SubscriptionRankingByUser;
+import br.com.nlw.events.dto.SubscriptionRankingItem;
 import br.com.nlw.events.dto.SubscriptionResponse;
 import br.com.nlw.events.exception.EventNotFoundException;
 import br.com.nlw.events.exception.SubscriptionConflictException;
@@ -41,11 +46,15 @@ public class SubscriptionService {
 			userRec = userRepo.save(user);
 		}
 		
-		User indicador = userRepo.findById(userId).orElse(null);
-		if (indicador == null) {
-			throw new UserIndicadorNotFoundException("Usuario " + userId + " indicador não existe");
-		}
+		User indicador = null;
 		
+		if (userId != null) {
+			indicador = userRepo.findById(userId).orElse(null);
+			if (indicador == null) {
+				throw new UserIndicadorNotFoundException("Usuario " + userId + " indicador não existe");
+			}
+		}
+				
 		Subscription subs = new Subscription();		
 		subs.setEvent(evt);
 		subs.setSubscriber(userRec);
@@ -60,6 +69,30 @@ public class SubscriptionService {
 		Subscription res = subRepo.save(subs);
 		
 		return new SubscriptionResponse(res.getSubscriptionNumber(), "http://codecraft.com/subscription" + res.getEvent().getPrettyName() + "/" + res.getSubscriber().getId());
+	}
+	
+	public List<SubscriptionRankingItem> getCompleteRanking(String prettyName) {
+		Event evt = evtRepo.findByPrettyName(prettyName);
+		if (evt == null) {
+			throw new EventNotFoundException("Ranking do evento "+prettyName+" nao existe");
+		}
+		return subRepo.generatedRanking(evt.getEventId());
+	}
+	
+	public SubscriptionRankingByUser getRankingByUser(String prettyName, Integer userId) {
+		List<SubscriptionRankingItem> ranking = getCompleteRanking(prettyName);
+		
+		SubscriptionRankingItem item = ranking.stream().filter(i -> i.userId().equals(userId)).findFirst().orElse(null);
+		
+		if (item == null) {
+			throw new UserIndicadorNotFoundException("Nao ha inscricoes com indicacao do usuario " + userId);
+		}
+		
+		Integer posicao = IntStream.range(0, ranking.size())
+				.filter(pos -> ranking.get(pos).userId().equals(userId))
+				.findFirst().getAsInt();
+		
+		return new SubscriptionRankingByUser(item, posicao + 1);
 	}
 	
 }
